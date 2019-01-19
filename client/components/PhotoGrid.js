a70b3fe1a679cbf5097fc05bb4ef0517dc9fedb7
Nikhil Namal <nikhilnamal16@gmail.com>
2019-01-20 00:53:45 +0530

    Add initial push

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+    render() {
+        return (
+            <div className="photo-grid">
+                <p>I'm Grid</p>
+            </div>
+        );
+    }
+}
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Single extends React.Component {
+    render() {
+        return (
+            <div className="single-photo">
+                <p>I'm Single</p>
+            </div>
+        );
+    }
+}
+
+export default Single;
